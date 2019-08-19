c37c83a383a25661302119ce539c8ad8ac8ef232
Basit.Abdulkareem <Basit.Abdulkareem@ISW-ENGR-BA>
2019-08-19 19:45:08 +0100

    Updates

---
+++ precache-manifest.d9ae5c0b2a11a98e9a46f2ab35aaf9f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7800450befdbfcba44e6a7990e88cbc5",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "225be328b052601ae23c",
+    "url": "/monster-rolodex/static/css/main.fe930664.chunk.css"
+  },
+  {
+    "revision": "cbfe3a179dd2d13b036c",
+    "url": "/monster-rolodex/static/js/2.c4624553.chunk.js"
+  },
+  {
+    "revision": "225be328b052601ae23c",
+    "url": "/monster-rolodex/static/js/main.9bb23925.chunk.js"
+  },
+  {
+    "revision": "c9aebef12b4044466a12",
+    "url": "/monster-rolodex/static/js/runtime~main.c182bce2.js"
+  }
+]);
